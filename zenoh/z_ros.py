c7d2f6d08525b31d3beac4c27dc1ec6685f3b089
Kyle Storey <kyle@kylona.com>
2025-04-05 12:50:51 -0400

    Add more zenoh experiments

--- zenoh/z_ros.py
+++ zenoh/z_ros.py
@@ -1,17 +1,13 @@
-import zenoh, random, time
+# Some required imports
+import zenoh
 
-random.seed()
+# Initiate the zenoh-net API
+config = zenoh.Config()
+with zenoh.open(config) as session:
+    # Declare the callback and the subscriber for Log messages with key 'rt/rosout'
+    def rosout_callback(sample):
+        print(f"PAYLOAD:{sample}")
 
-def read_temp():
-    return random.randint(15, 30)
+    sub = session.declare_subscriber('rt/rosout', rosout_callback)
 
-if __name__ == "__main__":
-    with zenoh.open(zenoh.Config()) as session:
-        key = 'myhome/kitchen/temp'
-        pub = session.declare_publisher(key)
-        while True:
-            t = read_temp()
-            buf = f"{t}"
-            print(f"Putting Data ('{key}': '{buf}')...")
-            pub.put(buf)
-            time.sleep(1)
+    session.put('rt/hello/message', "Hello World!")
